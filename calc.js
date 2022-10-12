b4f380d29efecf3d38574a90a8bc8cee81b5c86c
Payam Mohajerin Esfahani <payam3006@gmail.com>
2022-10-12 20:08:39 +0330

    whit monnitor functions first edition

--- calc.js
+++ calc.js
@@ -9,26 +9,27 @@ documentHeight();
 
 //Whirting on Monitor
 const whrite = (button) => {
-  let text = document.getElementById("monitor").innerText;
+  let text = document.getElementById("monitorText").innerText;
   if (text == "0") {
     text = "";
   }
   text += button;
-  document.getElementById("monitor").innerText = text;
+  document.getElementById("monitorText").innerText = text;
+  textModify();
 };
 
 //Clear Monitor
 const clearAll = () => {
-  document.getElementById("monitor").innerText = "0";
+  document.getElementById("monitorText").innerText = "0";
 };
 
 //back space
 const backSpace = () => {
-  let text = document.getElementById("monitor").innerText;
+  let text = document.getElementById("monitorText").innerText;
   if (text.length == 1) {
-    document.getElementById("monitor").innerText = "0";
+    document.getElementById("monitorText").innerText = "0";
   } else {
-    document.getElementById("monitor").innerText = text.substring(
+    document.getElementById("monitorText").innerText = text.substring(
       0,
       text.length - 1
     );
@@ -117,10 +118,54 @@ const onClickEffect = (character) => {
   setTimeout(function () {
     document
       .getElementById(`button${character}`)
-      .style.setProperty("opacity", "100%");
+      .style.setProperty("opacity", "");
   }, 150);
 };
 
 const openSettings = () => {
   document.getElementById("settingsMenue").style.display = "block";
+};
+
+//get the elemnt size in "...px" mode!
+const getFontSize = (elementId) => {
+  const textElement = document.getElementById(elementId);
+  const cssTextObj = window.getComputedStyle(textElement, null);
+  const fontSize = cssTextObj.getPropertyValue("font-size");
+  return fontSize;
+};
+
+const monitorTextLinesNum = () => {
+  const text = document.getElementById("monitorText").innerText;
+  console.log(text);
+};
+
+const monitorText = () => {
+  const text = document.getElementById("monitorText").innerText;
+  return text;
+};
+
+//analyse and modify monitor text displaying
+const textModify = () => {
+  //get the elemnt size!
+  const fontSize = getFontSize("monitorText");
+
+  //get the monitor (assume without padding(10px from left and right)) and text width
+  const monitorWidth = document.getElementById("monitor").offsetWidth - 20;
+  const spanWidth = document.getElementById("monitorText").offsetWidth;
+
+  //see how code is working!
+  console.log(
+    `fontSize=${fontSize} , monitorWidth=${monitorWidth} , spanWidth=${spanWidth} , monitorText=${monitorText()} , indexof=${document
+      .getElementById("monitorText")
+      .innerText.indexOf("<br>")}`
+  );
+
+  //main operations
+  if (spanWidth > monitorWidth) {
+    if (fontSize == "50px") {
+      document.getElementById("monitorText").style.fontSize = "35px";
+    } else {
+      document.getElementById("monitorText").innerText += "<br>";
+    }
+  }
 };
